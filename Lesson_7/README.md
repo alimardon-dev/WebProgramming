fcc3cd9c0aa260a4015cec638e81451df554e85a
Alimardon <asotvoldiev922@gmail.com>
2026-01-15 12:54:25 +0900

    lesson 7

---
+++ Lesson_7/README.md
@@ -0,0 +1,35 @@
+Arrays and their features
+Array index and values
+Array state properties
+
+there are two type of array methods
+    Mutable:
+        push() 
+            adds one item to the last part of the array
+        pop()
+            removes one item from the last part of the array
+        unshift()
+            add one item to the beginning part of the array
+        shift()
+            removes one item from beginning part of the array
+        splice()
+            it remove the items from array and puts new item
+    Immutable:
+        sort()
+            sort the array in ascending order
+        reverse()
+            reverse the array 
+        include()
+            shows if one items includes or not
+        indexOf()
+            shows the index of the item
+        toString()
+            converts the array to string 
+        join()
+            adds array elements to one string
+        slice()
+            it slice the array it returns a new list 
+        concat()
+            add two arrays to one array
+
+    
---
+++ Lesson_7/index.js
@@ -0,0 +1,59 @@
+//literal
+const list = [1,2,3,4,5,5,6];
+// console.log(list);
+
+// constructor
+const list2 = new Array("Apple", "Mango", "Banana");
+// console.log(list2);
+
+
+const fruits = ["apple", "melon", "cherry", "banana"]
+console.log(fruits);
+
+// Value: apple, melon, cherry, banana
+// Index: 0, 1, 2, 3
+
+// const fruit = fruits[0];
+// console.log(fruit);
+
+
+// property
+console.log(fruits.length);
+
+// Method: Regular methods
+// push() 
+    // adds one item to the last part of the array
+// pop()
+    // removes one item from the last part of the array
+// unshift()
+    // add one item to the beginning part of the array
+// shift()
+    // removes one item from beginning part of the array
+    // these methods were mutable methods
+// sort()
+    // sort the array in ascending order
+// reverse()
+    // reverse the array 
+// include()
+    // shows if one items includes or not
+// indexOf()
+    // shows the index of the item
+// toString()
+    // converts the array to string 
+// join()
+    // adds array elements to one string
+// slice()
+    // it slice the array it returns a new list 
+// splice()
+    // it remove the items from array and puts new item
+// concat()
+    // add two arrays to one array
+// these are immutable arrays(except splice)
+
+
+
+
+
+
+
+
