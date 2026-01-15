1cbb8c40ccf6bdc2f6ef22ba991a9c55c92b16bb
Alimardon <asotvoldiev922@gmail.com>
2026-01-15 18:40:55 +0900

    lesson 16

---
+++ Lesson_16/README.md
@@ -0,0 +1,17 @@
+Mainly about External Modules 
+
+External packages:
+    Moment 
+        shows current time
+    Inquirer
+        helps to interact with the user
+    Nodemon
+        runs the program everytime changes happen
+    Validator
+        checks for validity
+    typescript
+        better version of javascript. better to handle bugs before running
+    uuid
+        generates random string 
+    chalk 
+        changes the color of texts in terminal
---
+++ Lesson_16/index.js
@@ -0,0 +1,44 @@
+// const moment = require("moment");
+// const time = app.format("YYYY-MM-DD");
+
+
+// setInterval(() => { 
+//     const app = moment()
+//     const time = app.format("HH:mm:ss");
+//     console.log(time)
+// }, 500);
+// console.log(time);
+
+
+// import inquirer from 'inquirer';
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       name: "raqam",
+//       message: "raqamni kiriting:",
+//     },
+//   ])
+//   .then((answer) => {
+//     console.log("Raqam qiymati:", answer.raqam);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// validator
+
+// const validator = require("validator");
+// const validity = validator.isEmail("hello@gmail.com");
+// const validity2 = validator.isStrongPassword("$100daA4");
+// const validity3 = validator.isIP("100.100.100")
+
+// console.log(validity3);
+
+
+// // import {v4 as uuidv4, v4} from "uuid";
+// const { v4: uuidv4 } = require("uuid");
+// const random = uuidv4();  
+
+// console.log(random);
