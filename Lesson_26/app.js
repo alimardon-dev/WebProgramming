da0909e9e1f12f2b5be210e966eb8755a8adf90a
Alimardon <asotvoldiev922@gmail.com>
2026-01-19 14:05:40 +0900

    Lesson 26 CRUD

--- Lesson_26/app.js
+++ Lesson_26/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const { client } = require("./db/index");
-// Initializing db variable later to ensure client is connected
 let db;
 const getDB = () => {
   if (!db) db = client.db("Reja");
@@ -20,13 +19,9 @@ app.get("/", async (req, res) => {
     console.log("User entered / ");
     console.log("Fetching plans...");
 
-    // Use await instead of callback
-    // getting data from database 
     const data = await getDB().collection("plans_1").find().toArray();
     console.log("Plans fetched:", data);
 
-    // Render the view with the fetched data
-    // we made a variable called plans with value of data inside reja.ejs file
     res.render("reja", { plans: data });
   } catch (err) {
     console.log("Error fetching plans:", err);
@@ -34,36 +29,28 @@ app.get("/", async (req, res) => {
   }
 });
 
-// app.get("/author", (req, res) => {
-//   res.render("author", { user: { name: "Alimardon", secName: "Dev" } });
-// });
 
 
 // this route is inserting data into database
 app.post("/create-item", async (req, res) => {
   try {
-    // to know if user visited this page
     console.log("User entered /create-item");
-
-    // show the item created in input field
+    
     console.log("Create item request:", req.body);
-
-    // Insert the item into the database
-    // input field has name = reja
+    
     if (!req.body.reja) {
       return res.status(400).json({ error: "Item content is required" });
     }
-
-    // Inserting data into database using await
+    
     const result = await getDB().collection("plans_1").insertOne({ plan: req.body.reja });
-
+    
     // Respond with the newly created item
     // MongoDB v7 insertOne returns an object with insertedId
     const newPlan = {
       _id: result.insertedId,
       plan: req.body.reja
     };
-
+    
     console.log("New plan created:", newPlan);
     res.json(newPlan);
   } catch (err) {
@@ -72,5 +59,8 @@ app.post("/create-item", async (req, res) => {
   }
 });
 
+// app.get("/author", (req, res) => {
+//   res.render("author", { user: { name: "Alimardon", secName: "Dev" } });
+// });
 
 module.exports = app;
